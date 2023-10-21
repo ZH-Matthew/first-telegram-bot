af02bc6da62102a0f490118361cb60ab7da120d3
m.zhitenev <m.zhitenev23@gmail.com>
2023-10-21 19:07:17 +0300

    2.4 дописать метод чтобы он мог парсить сообщения с создаваемым напоминанием и сохранять их в БД

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -9,9 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import pro.sky.telegrambot.model.Notification;
+import pro.sky.telegrambot.repository.NotificationRepository;
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -26,17 +32,38 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
+    NotificationRepository notificationRepository;
+
+    public TelegramBotUpdatesListener(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            if (update.message().text().equals("/start")){
-                Long chatId = update.message().chat().id();
+            String userMessage = update.message().text();
+            Long chatId = update.message().chat().id();
+
+            //проверка на /start
+            if (userMessage.equals("/start")){
                 String messageText = "О! Привет!";
                 SendMessage message = new SendMessage(chatId, messageText);
                 SendResponse response = telegramBot.execute(message);
                 response.isOk();
                 response.errorCode();
+            }
+
+            //парсинг сообщения и сохранение в БД
+            Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");  //создали паттерн (условие поиска)
+            Matcher matcher = pattern.matcher(userMessage);                                 //проверили сообщение по условию (паттерну) на соответствие
+            if (matcher.matches()) {                                                        //если условие удовлетворено
+                String date = matcher.group(1);                                             //разделили сообщение по частям
+                LocalDateTime notification_send_time = LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+                String notification_message = matcher.group(3); //!!! узнать почему тут именно цифра 3
+
+                Notification notification = new Notification(chatId,notification_message,notification_send_time);
+                notificationRepository.save(notification);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
