0167bf1de87db2e203b1c2a907a3ef0bdfe61dbc
m.zhitenev <m.zhitenev23@gmail.com>
2023-10-21 17:43:58 +0300

    2.2 получить класс с аннотацией @Entity, который будет повторять структуру таблицы в БД и будет пригоден для использования в коде приложения.

---
+++ src/main/java/pro/sky/telegrambot/model/Notification.java
@@ -0,0 +1,49 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity(name = "notification_task")
+public class Notification {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long chat_id;
+    private String notification;
+    private LocalDateTime notification_send_time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChat_id() {
+        return chat_id;
+    }
+
+    public void setChat_id(Long chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public LocalDateTime getNotification_send_time() {
+        return notification_send_time;
+    }
+
+    public void setNotification_send_time(LocalDateTime notification_send_time) {
+        this.notification_send_time = notification_send_time;
+    }
+}
